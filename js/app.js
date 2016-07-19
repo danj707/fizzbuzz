082fb6a677b75ecde4bc09202dec80cf93b83ccc
Dan Jenner <danjenner@gmail.com>
2016-07-19 10:00:14 -0500

    Final fizzbuzz

--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
 $(document).ready(function () {
 
-alert("fizzbuzz!");
 
-
+    for (i = 1; i <= 100; i++) {
+        if ((i % 3 == 0) && (i % 5 == 0)) {
+            $('body').append('<p>fizzbuzz</p>')
+        } else if (i % 3 == 0) {
+            $('body').append('<p>fizz</p>')
+        } else if (i % 5 == 0) {
+            $('body').append('<p>buzz</p>')
+        } else {
+            $('body').append('<p>' + i + '</p>')
+        }
+    }
 });
