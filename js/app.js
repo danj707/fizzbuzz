98d7f23b969efe5de0d1788da26677374f243ad0
Dan Jenner <danjenner@gmail.com>
2016-07-20 12:16:42 -0500

    working version 2, accepts inputs, basic validation

--- js/app.js
+++ js/app.js
@@ -1,21 +1,30 @@
 $(document).ready(function () {
+    //add button clicker
+    $("#start_form").submit(function (event) {
+        event.preventDefault(); // dont submit to server
+        var fizzbuzzStart = parseInt($("input#start").val()); //grab val and get the integer
+        var fizzbuzzEnd = parseInt($("input#end").val()); //grab val end and get the int
 
-    //for loop, checking i, starting at 1, to see if modulus with 3, 5, or both
-    //equals 0
-
-    for (i = 1; i <= 100; i++) {
-        if ((i % 3 == 0) && (i % 5 == 0)) {
-            //first check both
-            $('body').append('<p>fizzbuzz</p>')
-        } else if (i % 3 == 0) {
-            //then check 3
-            $('body').append('<p>fizz</p>')
-        } else if (i % 5 == 0) {
-            //then check 5
-            $('body').append('<p>buzz</p>')
+        if (isNaN(fizzbuzzStart) || isNaN(fizzbuzzEnd)) {
+            //tried to enter a string, reprompt
+            alert("Good try, one of those wasn't a number.")
         } else {
-            //else must not be mod either, print number
-            $('body').append('<p>' + i + '</p>')
+            //both good numbers
+            for (i = fizzbuzzStart; i <= fizzbuzzEnd; i++) {
+                if ((i % 3 == 0) && (i % 5 == 0)) {
+                    //first check both
+                    $('body').append('<p>' + i + ' :fizzbuzz</p>')
+                } else if (i % 3 == 0) {
+                    //then check 3
+                    $('body').append('<p>' + i + ' : fizz</p>')
+                } else if (i % 5 == 0) {
+                    //then check 5
+                    $('body').append('<p>' + i + ' : buzz</p>')
+                } else {
+                    //else must not be mod either, print number
+                    $('body').append('<p>' + i + '</p>')
+                }
+            }
         }
-    }
+    });
 });
